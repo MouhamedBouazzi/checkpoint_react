e12183beccc2c85109e81584adfe30e6e56d78d0
unknown <mouhamed.bouazzi@gmail.com>
2022-01-03 09:17:33 +0000

    a

---
+++ src/App.js
@@ -0,0 +1,57 @@
+// to import the element from react bootstrap
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+
+// -------------------
+// to import the local CSS
+import "./App.css";
+
+// ----------------------------
+// to import each element alone from react-bootstrap
+// import Navbar from "react-bootstrap/Navbar";
+// import Nav from "react-bootstrap/Nav";
+// import Container from "react-bootstrap/Container";
+
+// ------------------------------------
+// to import all the element as X
+// import * as X from "react-bootstrap";
+
+function App() {
+  return (
+    // each function==component should return 1 element ( wrapped in div or <></>)
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Form className="form-container">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default App;
